9103cbe72562bdc38ca8d7a68efceb4bec83fd40
Marcos653 <marcos.costas@xbrain.com.br>
2024-02-11 16:31:25 -0300

    refactor: change interface name

--- src/main/java/authentication/infra/repository/UserRepositoryJpa.java
+++ src/main/java/authentication/infra/repository/UserRepositoryJpa.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IUserRepositoryImpl extends JpaRepository<User, Long>, IUserRepository {
+public interface UserRepositoryJpa extends JpaRepository<User, Long>, IUserRepository {
 }
